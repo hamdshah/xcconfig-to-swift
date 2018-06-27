29e1be2410997a4f3e2a2c942e30ec5f6772b65f
Shah <hamdullah.shah@affinitas.de>
2018-06-27 16:41:22 +0200

    initial commit

---
+++ MovieDB/AppDelegate.swift
@@ -0,0 +1,22 @@
+//
+//  AppDelegate.swift
+//  MovieDB
+//
+//  Created by Shah on 6/27/18.
+//  Copyright © 2018 shah. All rights reserved.
+//
+
+import UIKit
+
+@UIApplicationMain
+class AppDelegate: UIResponder, UIApplicationDelegate {
+
+    var window: UIWindow?
+
+
+    func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplicationLaunchOptionsKey: Any]?) -> Bool {
+        
+        return true
+    }
+}
+
